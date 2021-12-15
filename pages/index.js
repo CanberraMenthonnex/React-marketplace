0ee9e6863d0c9c53c67818c415ca18811763852e
Canberra <canberra.mc@gmail.com>
2021-12-15 14:49:09 +0100

    todo list: CRUD

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,26 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import React, {useState} from 'react';
 
 export default function Home() {
+
+    const [count, setCount] = useState(0);
+    const [todoList, setTodoList] = useState(["apple", "banana", "tamato"]);
+    const [addTodo, setAddTodo] = useState("");
+    const [deleteTodo, setDeleteValue] = useState("");
+    const [editabledTodo, setEditabledTodo] = useState(null);
+
+    const editTask = (item, index) => {
+        setEditabledTodo(index);
+    }
+
+    const handleChange = e => {
+
+        let newTodoList = [...todoList];
+        newTodoList[editabledTodo] = e.target.value;
+        setTodoList(newTodoList);
+    }
+
   return (
     <div className={styles.container}>
       <Head>
@@ -12,58 +30,34 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <h1>Hello there </h1>
+        <p>count : {count}</p>
+        <button onClick={() => setCount(prev => prev + 1)}>Ajouter</button>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+        <input onChange={(e) => setAddTodo(addTodo => e.target.value)}></input>
+        <button onClick={() => setTodoList(todoList => [...todoList, addTodo])}>Add todo</button>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+        { todoList.length > 0 && (
+            todoList.map((todo,i) =>{
+                return(
+                    <div key = {i}>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+                        <input type = "text" onChange={(e) => handleChange(e)} value={todo} disabled={editabledTodo === i ? false :true}/>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
+                        <button onClick={() => editTask(todo,i)}>Edit</button>
+                        <button onClick={()=>setTodoList(prevArray => prevArray.filter((item, index) => index !== i))}>Delete</button>
+                    </div>
+                )
+                
+            })
+        )}
+    
 
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
       </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
   )
 }
+
+
+
